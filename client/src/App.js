1d5ed6385326f3a91d677efb445f77d61ed026ee
John Zhang <John.Zhang3@ibm.com>
2020-08-24 18:15:39 -0300

    Complete app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 import "./App.css";
+import logo from "./logo.png";
 
-function App() {
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>SpaceX</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
